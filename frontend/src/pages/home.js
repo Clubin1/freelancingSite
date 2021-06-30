3ec6439ade9b17278b019051f3a5ca02a0b3b695
Justin Tubay <justin.tubay@gmail.com>
2021-06-30 01:55:50 -0400

    home page done with media queries

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,13 @@
 import HomeLanding from '../components/HomeLanding'
 import HomeAbout from '../components/HomeAbout'
 import ContactSection from '../components/ContactSection'
+import HomePricing from '../components/HomePricing'
 
 const Home = () => {
     return (<div>
         <HomeLanding/>
         <HomeAbout/>
+        <HomePricing/>
         <ContactSection/>
     </div>)
 }
